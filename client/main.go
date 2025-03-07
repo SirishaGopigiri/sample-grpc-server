a095cf4d48199aa504dfc1819db13ff6eb7532cd
Sirisha Gopigiri <sirishagopigiri@gmail.com>
2025-03-07 08:34:18 -0600

    Update client

--- client/main.go
+++ client/main.go
@@ -2,17 +2,23 @@ package main
 
 import (
 	"context"
+	"flag"
 	"log"
 	"time"
 
 	pb "github.com/SirishaGopigiri/sample-grpc-server/user"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+var (
+	addr = flag.String("addr", "localhost:50051", "the address to connect to")
 )
 
 func main() {
-	conn, err := grpc.NewClient("localhost:50051", grpc.WithInsecure())
+	conn, err := grpc.NewClient(*addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
-		log.Fatalf("failed to connect: %v", err)
+		log.Fatalf("did not connect: %v", err)
 	}
 	defer conn.Close()
 
@@ -21,7 +27,7 @@ func main() {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
 
-	resp, err := client.SayHello(ctx, &pb.UserRequest{Name: "Gopher"})
+	resp, err := client.SayHello(ctx, &pb.UserRequest{Name: "Sirisha", Age: 30, Email: "sirishatest"})
 	if err != nil {
 		log.Fatalf("could not greet: %v", err)
 	}
