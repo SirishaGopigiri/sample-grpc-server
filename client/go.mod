f860ed054db34342583db0af02b67fa4ff2ee360
Sirisha Gopigiri <sirishagopigiri@gmail.com>
2025-03-09 14:51:35 -0500

    Update client

--- client/go.mod
+++ client/go.mod
@@ -3,11 +3,11 @@ module example.com/client
 go 1.23.5
 
 require (
-	github.com/SirishaGopigiri/sample-grpc-server v0.1.0 // indirect
-	golang.org/x/net v0.34.0 // indirect
-	golang.org/x/sys v0.29.0 // indirect
-	golang.org/x/text v0.21.0 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20250115164207-1a7da9e5054f // indirect
+	github.com/SirishaGopigiri/sample-grpc-server v0.3.0 // indirect
+	golang.org/x/net v0.37.0 // indirect
+	golang.org/x/sys v0.31.0 // indirect
+	golang.org/x/text v0.23.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20250303144028-a0af3efb3deb // indirect
 	google.golang.org/grpc v1.71.0 // indirect
 	google.golang.org/protobuf v1.36.5 // indirect
 )
--- client/main.go
+++ client/main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"context"
 	"flag"
+	"fmt"
+	"io"
 	"log"
+	"sync"
 	"time"
 
 	pb "github.com/SirishaGopigiri/sample-grpc-server/user"
@@ -15,6 +18,8 @@ var (
 	addr = flag.String("addr", "localhost:50051", "the address to connect to")
 )
 
+var wg sync.WaitGroup
+
 func main() {
 	conn, err := grpc.NewClient(*addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
@@ -23,14 +28,113 @@ func main() {
 	defer conn.Close()
 
 	client := pb.NewUsersClient(conn)
-
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	resp, err := client.SayHello(ctx, &pb.UserRequest{Name: "Sirisha", Age: 30, Email: "sirishatest"})
-	if err != nil {
-		log.Fatalf("could not greet: %v", err)
+	for {
+		fmt.Println("GRPC Client with options:")
+		fmt.Println("1. Stream Request")
+		fmt.Println("2. Stream Response")
+		fmt.Println("3. Stream Req Response")
+		fmt.Println("0. Exit")
+		fmt.Print("Please select and option from above: ")
+		var value int
+		fmt.Scan(&value)
+		switch value {
+		case 1:
+			fmt.Println("Sending Stream Request")
+			SendStreamrequest(ctx, client)
+		case 2:
+			fmt.Println("Sending Stream Response Request")
+			SendReqStreamResp(ctx, client)
+		case 3:
+			fmt.Println("Sending Stream Request and getting Stream Response:")
+			SendReqRespClient(ctx, client)
+		case 0:
+			return
+		default:
+			fmt.Println("Unknown input, please select proper input!")
+		}
 	}
-	log.Println("Resp: ", resp)
-	log.Println("Greeting:", resp.Message)
+}
+
+func SendReqStreamResp(ctx context.Context, client pb.UsersClient) {
+	fmt.Println("Enter name to fetch:")
+	var name string
+	fmt.Scan(&name)
+	user := &pb.UserReq{Name: name}
+	stream, err := client.StreamResponse(ctx, user)
+	if err != nil {
+		log.Fatalf("Error calling StreamReqResp: %v", err)
+	}
+	for {
+		msg, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalf("Error receiving stream: %v", err)
+		}
+		time.Sleep(1 * time.Second)
+		fmt.Println("Server Response:", msg.GetMessage())
+	}
+}
+
+func SendStreamrequest(ctx context.Context, client pb.UsersClient) {
+	stream, err := client.StreamRequest(ctx)
+	if err != nil {
+		log.Fatalf("Error calling StreamReqResp: %v", err)
+	}
+	for {
+		fmt.Println("Enter name to fetch:")
+		var name string
+		fmt.Scan(&name)
+		if name == "exit" {
+			break
+		}
+		stream.Send(&pb.UserReq{Name: name})
+		time.Sleep(time.Second)
+	}
+	resp, err := stream.CloseAndRecv()
+	if err != nil {
+		log.Fatalf("Error receiving response: %v", err)
+	}
+	fmt.Println("Server Response:", resp.GetMessage())
+}
+
+func SendReqRespClient(ctx context.Context, client pb.UsersClient) {
+	stream, err := client.StreamReqResp(ctx)
+	if err != nil {
+		log.Fatalf("Error calling StreamReqResp: %v", err)
+	}
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for {
+			fmt.Println("Enter name to fetch:")
+			var name string
+			fmt.Scan(&name)
+			if name == "exit" {
+				stream.CloseSend()
+				return
+			}
+			stream.Send(&pb.UserReq{Name: name})
+			time.Sleep(time.Second)
+		}
+	}()
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for {
+			msg, err := stream.Recv()
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				log.Fatalf("Error receiving: %v", err)
+			}
+			fmt.Println("Server:", msg.GetMessage())
+		}
+	}()
+	wg.Wait()
 }
